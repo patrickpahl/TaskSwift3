78d940a3e795a4049e649c3cab3dd42165b5380b
Patrick Pahl <ppahl84@yahoo.com>
2016-10-17 18:36:21 -0600

    Completed

--- TaskSwift3/DetailViewController.swift
+++ TaskSwift3/DetailViewController.swift
@@ -9,21 +9,40 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     var task: Task?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateWith()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var taskTextView: UITextView!
+    
+    @IBAction func saveButtonTapped(_ sender: AnyObject) {
+        
+        if let taskName = taskTextField.text, taskName != "" {
+            if let task = task {
+                TaskController.sharedController.update(task: task, title: taskName, notes: taskTextView.text, due: dueDatePicker.date)
+            } else {
+                let _ = TaskController.sharedController.createTask(title: taskName, notes: taskTextView.text, due: dueDatePicker.date, isComplete: false)
+            }
+            let _ = navigationController?.popToRootViewController(animated: true)
+        }
     }
-    */
-
+    
+    @IBAction func userTappedView(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func updateWith() {
+        guard let task = task else {return}
+        taskTextField.text = task.title
+        taskTextView.text = task.notes
+        if let due = task.due {
+            dueDatePicker.date = due as Date
+        }
+    }
 }
